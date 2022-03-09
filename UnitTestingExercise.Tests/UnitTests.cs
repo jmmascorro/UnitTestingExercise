86c72ddadc55be6fe85bbc4a3645e0b464d41a15
jmmascorro <jessemascorro88@gmail.com>
2022-03-09 07:06:50 -0600

    FinishedUnitTestingExercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,78 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
+        [InlineData(5,5,5,15)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,5,5)]
+        [InlineData(12,6,6)]
+        [InlineData(5,5,0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,10,100)]
+        [InlineData(5,5,25)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(6,6,1)]
+        [InlineData(30,5,6)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void GetHelloWorld()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.GetHelloWorld();
             //Assert
-
+            Assert.Equal("Hello World!", actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void GetColorOfSky()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.GetColorOfSky();
             //Assert
+            Assert.Equal("blue", actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,23 +4,48 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minued, int subtrhend)
+        {
+            return minued - subtrhend;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
 
         // Create a Divide method that passes 2 integers
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
 
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+        public string GetHelloWorld()
+        {
+            return "Hello World!";
+        }
+
+        public string GetColorOfSky()
+        {
+            return "blue";
+        }
     }
 }
